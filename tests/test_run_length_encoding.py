5bfa3fbbab76edc23e002d50793f435ad363b0e0
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-06-02 23:10:28 +0200

    First hypothesis breaks and rocks!
    Output:
    Falsifying example: test_with_property(
        sample='',
    )

--- tests/test_run_length_encoding.py
+++ tests/test_run_length_encoding.py
@@ -1,8 +1,18 @@
+from hypothesis import given
+import hypothesis.strategies as st
+
 from proptests.run_length_encoding import encode, decode
 
 
 def test_with_example():
     sample = "aksjdnkansdan"
+    output = decode(encode(sample))
+
+    assert sample == output
+
+
+@given(st.text())
+def test_with_property(sample):
     output = decode(encode(sample))
 
     assert sample == output
